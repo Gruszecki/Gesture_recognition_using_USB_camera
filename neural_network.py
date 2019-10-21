0daf8f5577ebd4670ff5e0d6df16656f943e723e
Gruszecki <wojtek.gruszecki@gmail.com>
2019-10-21 17:23:06 +0200

    Rollback to the version with three gestures and stable recognition square.

--- neural_network.py
+++ neural_network.py
@@ -6,28 +6,31 @@ import os
 import cv2
 import re
 
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+# alist.sort(key=natural_keys) sorts in human order
+def natural_keys(text):
+    return [atoi(c) for c in re.split('(\d+)', text)]
+
 def nn_go():
     print("Preparing arrays...")
     train_images_folder_path = "C:\\Users\\wojte\\Desktop\\Praca_inz\\train_images"
     train_images_paths = glob(os.path.join(train_images_folder_path, "*.jpg"))
-    train_images = np.array([[[0] * 38] * 38] * 1345, dtype=np.uint8)
-    train_images_labels = np.array([0] * 1345)
+    train_images = np.array([[[0] * 38] * 38] * 1937, dtype=np.uint8)
+    train_images_labels = np.array([0] * 1937)
+
     train_noise_folder_path = "C:\\Users\\wojte\\Desktop\\Praca_inz\\train_noise"
     train_noise_paths = glob(os.path.join(train_noise_folder_path, "*.jpg"))
-    train_noise = np.array([[[0]*38]*38]*6000, dtype=np.uint8)
-    train_hand = np.array([[[0]*38]*38]*7345, dtype=np.uint8)
-    train_hand_labels = np.array([0]*7345)
+    train_noise = np.array([[[0]*38]*38]*1937, dtype=np.uint8)
+
+    train_hand = np.array([[[0]*38]*38]*3874, dtype=np.uint8)
+    train_hand_labels = np.array([0]*3874)
 
     img_path = "C:\\Users\\wojte\\Desktop\\Praca_inz\\test_images"
     img_paths = glob(os.path.join(img_path, "*.jpg"))
     img_test = np.array([[[0] * 38] * 38] * 9, dtype=np.uint8)
-
-    def atoi(text):
-        return int(text) if text.isdigit() else text
-
-    # alist.sort(key=natural_keys) sorts in human order
-    def natural_keys(text):
-        return [atoi(c) for c in re.split('(\d+)', text)]
 
     # Sort paths in their arrays
     print("Sorting paths...")
@@ -44,7 +47,7 @@ def nn_go():
     # Converting noise from jpg to numpy
     for i in range(len(train_noise_paths)):
         train_noise[i] = cv2.cvtColor(cv2.imread(train_noise_paths[i]), cv2.COLOR_BGR2GRAY)
-        train_hand[1345+i] = train_noise[i]
+        train_hand[1937+i] = train_noise[i]
 
     # Converting test images from jpg to numpy
     for i in range(len(img_paths)):
@@ -52,18 +55,22 @@ def nn_go():
 
     # Creating labels for train images: numpy array with values 0-2
     print("Creating labels...")
-    label_value = 0
+
     for i in range(len(train_images_labels)):
-        if i == 739:
-            label_value = label_value + 1
-        train_images_labels[i] = label_value
+        if i < 739:
+            train_images_labels[i] = 0
+        elif i < 1345:
+            train_images_labels[i] = 1
+        else:
+            train_images_labels[i] = 2
+
 
     # Creating labels for train hand: numpy array with values 0-1
-    label_value = 1
     for i in range(len(train_hand_labels)):
-        if i == 1345:
-            label_value = 0
-        train_hand_labels[i] = label_value
+        if i < 1937:
+            train_hand_labels[i] = 0
+        else:
+            train_hand_labels[i] = 1
 
     print("Scaling data...")
     train_images = train_images / 255.0
@@ -71,20 +78,24 @@ def nn_go():
 
     #############################################################
     # Hand recognition training
-    print("Training...")
+    print("Preparing trainings...")
 
-    layer_conv2d_2_1 = keras.layers.Conv2D(8, (4, 4), input_shape=(38, 38, 1), activation=tf.nn.relu)
-    layer_conv2d_2_2 = keras.layers.Conv2D(16, (4, 4), activation=tf.nn.relu)
-    layer_maxpooling2d_2_1 = keras.layers.MaxPooling2D(pool_size=(1, 1))
+    layer_conv2d_2_1 = keras.layers.Conv2D(8, (8, 8), input_shape=(38, 38, 1), activation=tf.nn.relu)
+    layer_conv2d_2_2 = keras.layers.Conv2D(16, (8, 8), activation=tf.nn.relu)
+    layer_maxpooling2d_2_1 = keras.layers.MaxPooling2D(pool_size=(8, 8))
+    layer_maxpooling2d_2_2 = keras.layers.MaxPooling2D(pool_size=(2, 2))
+    layer_maxpooling2d_2_3 = keras.layers.MaxPooling2D(pool_size=(1, 1))
     layer_flatten_2_1 = keras.layers.Flatten()
     layer_dense_2_1 = keras.layers.Dense(50, activation=tf.nn.relu)
-    layer_dense_2_2 = keras.layers.Dense(50, activation=tf.nn.relu)
+    layer_dense_2_2 = keras.layers.Dense(20, activation=tf.nn.relu)
     layer_dense_2_3 = keras.layers.Dense(2, activation=tf.nn.softmax)
 
     model_h = keras.Sequential([
         layer_conv2d_2_1,
         layer_conv2d_2_2,
         layer_maxpooling2d_2_1,
+        layer_maxpooling2d_2_2,
+        layer_maxpooling2d_2_3,
         layer_flatten_2_1,
         layer_dense_2_1,
         layer_dense_2_2,
@@ -95,18 +106,14 @@ def nn_go():
                     loss='sparse_categorical_crossentropy',
                     metrics=['accuracy'])
 
-    train_hand.resize([7345, 38, 38, 1])
-    model_h.fit(train_hand, train_hand_labels, epochs=1)
-
-    # Saving entire model to a HDF5 file
-    # model_h.save('F:\\PyCharm 5.0.4\\PROJEKTY\\ExternalCamera\\model_h.h5')
+    train_hand.resize([3874, 38, 38, 1])
 
     ##############################################################
     # Gesture recognition training
 
-    layer_conv2d_1 = keras.layers.Conv2D(32, (4, 4), input_shape=(38, 38, 1), activation=tf.nn.relu)
-    layer_conv2d_2 = keras.layers.Conv2D(64, (4, 4), activation=tf.nn.relu)
-    layer_maxpooling2d = keras.layers.MaxPooling2D(pool_size=(1, 1))
+    layer_conv2d_1 = keras.layers.Conv2D(8, (4, 4), input_shape=(38, 38, 1), activation=tf.nn.relu)
+    layer_conv2d_2 = keras.layers.Conv2D(4, (4, 4), activation=tf.nn.relu)
+    layer_maxpooling2d = keras.layers.MaxPooling2D(pool_size=(2, 2))
     layer_flatten = keras.layers.Flatten()
     layer_dense_1 = keras.layers.Dense(50, activation=tf.nn.relu)
     layer_dense_2 = keras.layers.Dense(50, activation=tf.nn.relu)
@@ -126,21 +133,24 @@ def nn_go():
                     loss='sparse_categorical_crossentropy',
                     metrics=['accuracy'])
 
-    train_images.resize([1345, 38, 38, 1])
+    train_images.resize([1937, 38, 38, 1])
     img_test.resize([9, 38, 38, 1])
+
+    ########################### T R A I N I N G #######################################
+
+    model_h.fit(train_hand, train_hand_labels, epochs=3)
     model_g.fit(train_images, train_images_labels, epochs=1)
 
     # Saving entire model to a HDF5 file
+    # model_h.save('F:\\PyCharm 5.0.4\\PROJEKTY\\ExternalCamera\\model_h.h5')
     # model_g.save('F:\\PyCharm 5.0.4\\PROJEKTY\\ExternalCamera\\model_g.h5')
 
-    #############################################################
-    # Summary
+    ############################# Summary ####################################
 
     model_h.summary()
     model_g.summary()
 
-    ##############################################################
-    # Tests
+    ########################## Tests ########################
 
     img_test = img_test / 255.0
     prediction_frame_h = model_h.predict(img_test)
